757a2f360cb9f54fd225450eb2c1683b636d5d4f
Matias Vazquez-Levi <matiasvlevi@gmail.com>
2022-01-16 06:49:54 -0500

    Update clean.bat

--- clean.bat
+++ clean.bat
@@ -1,9 +1,7 @@
+@echo off
 del /F /Q clean.sh index.js
 rmdir /S /Q node_modules
 rmdir /S /Q .git
-echo "Cleaned all files"
-pause
-
 call :deleteSelf&exit /b
 :deleteSelf
 start /b "" cmd /c del "%~f0"&exit /b
